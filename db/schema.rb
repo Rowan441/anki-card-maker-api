d5e9f345faef16e70a59daa702f3b31a95306860
Rowan Monk <63263642+Rowan441@users.noreply.github.com>
2025-09-23 07:19:22 +0000

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_23_044819) do
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "token", null: false
+    t.datetime "last_used_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid", "email"], name: "index_users_on_provider_and_uid_and_email", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
